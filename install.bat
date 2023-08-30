00d200dd39b25142fedab30b4f5cd53666916251
Danil Boldyrev <daswerq123@gmail.com>
2023-08-30 16:40:50 +0300

    Add extra files

--- install.bat
+++ install.bat
@@ -5,3 +5,5 @@ call venv/scripts/activate
 
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 pip install -r requerments.txt
+
+python dowload_rvc_base.py
